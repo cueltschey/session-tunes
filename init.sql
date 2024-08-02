eb3587b5de31c2c8805cd961c8f5fd48979054dc
Charles Ueltschey <chaseuelt@gmail.com>
2024-08-02 15:26:25 -0500

    create s starting database schema

---
+++ init.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Session (
+  session_id INTEGER PRIMARY KEY,
+  location_id INTEGER,
+  session_date DATE,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP,
+  description TEXT,
+  FOREIGN KEY (location_id) REFERENCES Location(location_id)
+);
+
+CREATE TABLE Location (
+  location_id INTEGER PRIMARY KEY,
+  description TEXT,
+  address VARCHAR(255),
+  url VARCHAR(255)
+);
+
+CREATE TABLE SetTable (
+  set_id INTEGER PRIMARY KEY,
+  description TEXT -- OPTIONAL removed as it's not a valid keyword in SQL
+);
+
+CREATE TABLE Tune (
+  tune_id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  abc_path VARCHAR(255),
+  key VARCHAR(255)
+);
+
+CREATE TABLE SetToSession (
+  session_id INTEGER,
+  set_id INTEGER,
+  set_index INTEGER,
+  FOREIGN KEY (session_id) REFERENCES Session(session_id),
+  FOREIGN KEY (set_id) REFERENCES SetTable(set_id)
+);
+
+CREATE TABLE TuneToSet (
+  tune_id INTEGER,
+  set_id INTEGER,
+  tune_index INTEGER,
+  FOREIGN KEY (tune_id) REFERENCES Tune(tune_id),
+  FOREIGN KEY (set_id) REFERENCES SetTable(set_id)
+);
